1217f8078547e2e3d4f3899851ca4da69e2e90dc
kass8mal8 <kassimaly21@gmail.com>
2022-05-25 11:22:26 +0300

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faBars,faCartShopping, faClose } from '@fortawesome/free-solid-svg-icons' 
+import "./style.css"
+
+const Header =()=>{
+  const avatar="https://aakashhelloworld.github.io/css2/images/avatar-shanai.png"
+
+  const handleToggle=()=>{
+    document.getElementById("toggle").classList.toggle("active")
+  }
+  const handleCart=()=>{
+       return(
+        <div className="h-cart">
+            <div className="h-cart--">
+           
+            </div>
+        </div>
+       ) 
+  }
+  return(
+    <div>
+        <div className="h-div">
+        <FontAwesomeIcon icon={faBars} className="h-div--bars" onClick={handleToggle}></FontAwesomeIcon><h3>sneakers</h3>
+        <div className="top-nav" id="toggle">
+          <FontAwesomeIcon icon={faClose} className="top-nav--close" onClick={handleToggle}></FontAwesomeIcon>
+        <ul className="top-nav--wrapper">
+          <li>collections</li>
+          <li>men</li>
+          <li>women</li>
+          <li>about</li>
+          <li>contacts</li>
+
+        </ul>
+        </div>
+        <FontAwesomeIcon icon={faCartShopping} className="h-div--cart" onClick={handleCart}></FontAwesomeIcon>
+        <img src={avatar} width="50px" alt="avatar"/>
+        </div>
+    </div>
+  )
+}
+export default Header
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from "react"
+const Main=()=>{
+  const image="https://lvcas-ca.github.io/E-commerce-product-page/images/image-product-1.jpg"
+  return(
+    <div className="m-div">
+        <div className="m-div--image">
+          <img src={image} />
+        </div>
+    </div>
+  )
+}
+export default Main
