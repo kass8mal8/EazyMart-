1e360500035c64b3a3bae9386d15a0e5965cdf7f
kass8mal8 <kassimaly21@gmail.com>
2022-05-25 21:57:54 +0300

    2 files modified

--- src/Smartphones.js
+++ src/Smartphones.js
@@ -4,18 +4,23 @@ const Smartphones=()=>{
   const smartphone =[
     {src:"https://www.pakmobizone.pk/wp-content/uploads/2020/11/Infinix-Zero-8-Green-Diamond-1.jpg",name:"Infinix Zero 8",price:10200,id:1 }, 
     {src:"https://www.pakmobizone.pk/wp-content/uploads/2020/09/infinix-Hot-10-Ocean-Wave-5.jpg",name:"Infinix Hot 10",price:8999,id:2}, 
-    {src:"https://acabuy.com/wp-content/uploads/2020/08/Huawei-P40-Pro.jpg ", name:"Huawei P40 pro",price:11100,id:3},
     
   ] 
- 
+
     const Iphone ="https://d30skdkspqmhrr.cloudfront.net/3VEa.jpg"
+    const Huawei="https://acabuy.com/wp-content/uploads/2020/08/Huawei-P40-Pro.jpg "
     
 
   return (
     <div className="smartphones">
+      <div>
        <img src={Iphone} width="170"/>
        <h5>IPhone 12</h5>
-
+      </div>
+      <div>
+        <img src={Huawei} width="170" />
+        <h5>Huawei P40 Pro</h5>
+      </div>
     </div>
   )
 }
