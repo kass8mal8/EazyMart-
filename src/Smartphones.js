0926efaac73d0bde24f66e04d9213b1d8abf656f
kass8mal8 <kassimaly21@gmail.com>
2022-05-25 19:18:41 +0300

    2 files modified

--- src/Smartphones.js
+++ src/Smartphones.js
@@ -3,7 +3,8 @@ import SmartphonesList from "./SmartphonesList"
 
 const Smartphones=()=>{
   const smartphone =[
-    {src:"https://www.pakmobizone.pk/wp-content/uploads/2020/11/Infinix-Zero-8-Green-Diamond-1.jpg",name:"Infinix 08",price:10200,id:1 }
+    {src:"https://www.pakmobizone.pk/wp-content/uploads/2020/11/Infinix-Zero-8-Green-Diamond-1.jpg",name:"Infinix 08",price:10200,id:1 }, 
+    {src:"https://www.pakmobizone.pk/wp-content/uploads/2020/09/infinix-Hot-10-Ocean-Wave-5.jpg",name:"Infinix Hot 10",price:8999,id:2}
     
   ] 
   return (
--- src/SmartphonesList.js
+++ src/SmartphonesList.js
@@ -5,7 +5,6 @@ const SmartphonesList =({smartphone})=>{
     <div>
       {smartphone.map(smartphone=>
         (<div className="smartphone-product">
-          <h3>Smartphones</h3>
           <img src={smartphone.src} width="170" />
           <h4>{smartphone.name} </h4>
         </div>)
