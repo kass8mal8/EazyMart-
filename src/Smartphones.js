4faacc2f2fe97f26a75a3f88f39731ddbd84b704
kass8mal8 <kassimaly21@gmail.com>
2022-05-25 19:34:46 +0300

    1 file modified

--- src/Smartphones.js
+++ src/Smartphones.js
@@ -4,7 +4,8 @@ import SmartphonesList from "./SmartphonesList"
 const Smartphones=()=>{
   const smartphone =[
     {src:"https://www.pakmobizone.pk/wp-content/uploads/2020/11/Infinix-Zero-8-Green-Diamond-1.jpg",name:"Infinix Zero 8",price:10200,id:1 }, 
-    {src:"https://www.pakmobizone.pk/wp-content/uploads/2020/09/infinix-Hot-10-Ocean-Wave-5.jpg",name:"Infinix Hot 10",price:8999,id:2}
+    {src:"https://www.pakmobizone.pk/wp-content/uploads/2020/09/infinix-Hot-10-Ocean-Wave-5.jpg",name:"Infinix Hot 10",price:8999,id:2}, 
+    {src:"https://acabuy.com/wp-content/uploads/2020/08/Huawei-P40-Pro.jpg ", name:"Huawei P40 pro",price:11100,id:3}
     
   ] 
   return (
