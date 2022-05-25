bccb34d325851adbeeed61b15a07818dec13a082
kass8mal8 <kassimaly21@gmail.com>
2022-05-25 18:21:46 +0300

    1 file modified

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Smartphones from "./Smartphones "
+
 const Main=()=>{
   return(
     <div className="container">
-       
+       <Smartphones />
     </div>   
   )
 }
