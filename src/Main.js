2d906be9a272d9968767c599053d3d63c995771d
kass8mal8 <kassimaly21@gmail.com>
2022-05-25 11:24:29 +0300

    1 file modified

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,8 @@
 import React from "react"
 const Main=()=>{
-  const image="https://lvcas-ca.github.io/E-commerce-product-page/images/image-product-1.jpg"
   return(
-    <div className="m-div">
-        <div className="m-div--image">
-          <img src={image} />
-        </div>
-    </div>
+    <div>
+     </div>   
   )
 }
 export default Main
