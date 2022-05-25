2e73c1def44ec30953b6cf7dc230e3ea101eb4c5
kass8mal8 <kassimaly21@gmail.com>
2022-05-25 17:58:48 +0300

    2 files modified

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,9 @@
 import React from "react"
 const Main=()=>{
   return(
-    <div>
-     </div>   
+    <div className="container">
+       <p>Hello world</p>
+    </div>   
   )
 }
 export default Main
