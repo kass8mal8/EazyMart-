5907ca5cf9b2201010aad7ba4f45b158873aadf5
kass8mal8 <kassimaly21@gmail.com>
2022-05-25 21:45:37 +0300

    2 files modified, 1 deleted

--- src/Smartphones.js
+++ src/Smartphones.js
@@ -1,19 +1,21 @@
 import React from 'react'
-import SmartphonesList from "./SmartphonesList"
 
 const Smartphones=()=>{
   const smartphone =[
     {src:"https://www.pakmobizone.pk/wp-content/uploads/2020/11/Infinix-Zero-8-Green-Diamond-1.jpg",name:"Infinix Zero 8",price:10200,id:1 }, 
     {src:"https://www.pakmobizone.pk/wp-content/uploads/2020/09/infinix-Hot-10-Ocean-Wave-5.jpg",name:"Infinix Hot 10",price:8999,id:2}, 
     {src:"https://acabuy.com/wp-content/uploads/2020/08/Huawei-P40-Pro.jpg ", name:"Huawei P40 pro",price:11100,id:3},
-    {src:"https://d30skdkspqmhrr.cloudfront.net/3VEa.jpg",name:"IPhone 12",price:17000,id:4}
     
   ] 
-
+ 
+    const Iphone ="https://d30skdkspqmhrr.cloudfront.net/3VEa.jpg"
+    
 
   return (
     <div className="smartphones">
-        <SmartphonesList smartphone={smartphone} />
+       <img src={Iphone} width="170"/>
+       <h5>IPhone 12</h5>
+
     </div>
   )
 }
