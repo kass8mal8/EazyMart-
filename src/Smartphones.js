81d74d62315bbf7927980d4ae733c119fd071b28
kass8mal8 <kassimaly21@gmail.com>
2022-05-25 22:43:37 +0300

    2 files modified

--- src/Smartphones.js
+++ src/Smartphones.js
@@ -2,14 +2,16 @@ import React from 'react'
 
 const Smartphones=()=>{
   const smartphone =[
-    {src:"https://www.pakmobizone.pk/wp-content/uploads/2020/11/Infinix-Zero-8-Green-Diamond-1.jpg",name:"Infinix Zero 8",price:10200,id:1 }, 
-    {src:"https://www.pakmobizone.pk/wp-content/uploads/2020/09/infinix-Hot-10-Ocean-Wave-5.jpg",name:"Infinix Hot 10",price:8999,id:2}, 
     
   ] 
 
     const Iphone ="https://d30skdkspqmhrr.cloudfront.net/3VEa.jpg"
     const Huawei="https://acabuy.com/wp-content/uploads/2020/08/Huawei-P40-Pro.jpg "
     
+    const Hot10="https://www.pakmobizone.pk/wp-content/uploads/2020/09/infinix-Hot-10-Ocean-Wave-5.jpg"
+  
+    const zero8="https://www.pakmobizone.pk/wp-content/uploads/2020/11/Infinix-Zero-8-Green-Diamond-1.jpg"
+
 
   return (
     <div className="smartphones-container">
@@ -17,13 +19,21 @@ const Smartphones=()=>{
       <div className="smartphones">
       <div>
        <img src={Iphone} width=""/>
-       <h5>IPhone 12  </h5>
-       <h5>ksh 17000</h5>
+       <h5>IPhone 12 
+       <br/>ksh 17000</h5>
       </div>
       <div>
-        <img src={Huawei} width="" />
-        <h5>Huawei P40 Pro</h5>
-        <h5>ksh 16549</h5>
+        <img src={Huawei} width="200" />
+        <h5>Huawei P40 Pro
+        <br/>ksh 16549</h5>
+      </div>
+      <div>
+        <img src={Hot10} alt=""/>
+        <h5>Infinix Hot 10<br/>ksh 8999</h5>
+      </div>
+      <div>
+        <img src={zero8}/>
+        <h5>Infinix 08 <br/>ksh 9499</h5>
       </div>
       </div>
     </div>
