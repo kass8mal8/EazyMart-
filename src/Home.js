abbdc456d602a6f80301adfe343945ba20eb6876
kass8mal8 <kassimaly21@gmail.com>
2022-05-26 21:42:22 +0300

    2 files modified

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home=()=>{
+  return(
+    <div className="home-container">
+       <div class="announcement">
+         <p>Free shipping on all goods above ksh 20000</p>
+       </div>
+    </div>
+  )
+}
+export default Home
