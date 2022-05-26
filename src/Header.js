1a7c4aea9846d8a1b6c60d3b9775586bcf36fcd1
kass8mal8 <kassimaly21@gmail.com>
2022-05-26 19:26:06 +0300

    1 file modified

--- src/Header.js
+++ src/Header.js
@@ -21,13 +21,11 @@ const Header =()=>{
           <FontAwesomeIcon icon={faClose} className="top-nav--close" onClick={handleToggle}></FontAwesomeIcon>
         <ul className="top-nav--wrapper">
           <li><Link to="/" >Home</Link></li>
-          <li>smartphones</li>
-          <li>foodstuff</li>
-          <li>home accessories </li>
-          <li>about</li>      
+          <li><Link to="/products" >products</Link></li>
+          <li><Link to="/about">about</Link></li>      
         </ul>
         </div>
-        <FontAwesomeIcon icon={faCartShopping} className="h-div--cart" onClick={handleCart}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={faCartShoppingOu} className="h-div--cart" onClick={handleCart}></FontAwesomeIcon>
         <img src={avatar} width="50px" alt="avatar"/>
         <FontAwesomeIcon icon={faUserCircle}className="h-div-user"></FontAwesomeIcon>
         </div>
