62211f6e312aefcb2427fc76956a1948eb228c69
kass8mal8 <kassimaly21@gmail.com>
2022-05-26 22:06:45 +0300

    1 file modified

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 const Home=()=>{
+  const image="https://clipground.com/images/clothes-png-for-picsart-5.png"
   return(
     <div className="home-container">
        <div class="announcement">
-         <p>Free shipping on all goods above ksh 20000</p>
+         <p>Free shipping on all goods above ksh 20,000</p>
+       </div>
+       <div className="slider">
+          <img src={image} />
+       </div > 
+       <div>
+       <p>We have a variety of products on sale for every person,at EazyMart prices are affordable so that nothing ruins your preferences.<br/> Glad to have you shop with us. 
+       </p>
+         
        </div>
     </div>
   )
