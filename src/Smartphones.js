86502c56fe78fc85a315eaa368c4d54d56d1aff6
kass8mal8 <kassimaly21@gmail.com>
2022-05-26 07:55:00 +0300

    1 file modified

--- src/Smartphones.js
+++ src/Smartphones.js
@@ -12,33 +12,38 @@ const Smartphones=()=>{
   
     const zero8="https://www.pakmobizone.pk/wp-content/uploads/2020/11/Infinix-Zero-8-Green-Diamond-1.jpg"
 
-  const handleClick=()=>{
-    document.querySelector('.product-page').style.display="block"
-    document.querySelector('.product-page').src=document.getAttribute(src)
+  const handleClick=(e)=>{
+      document.querySelector('.product-page').style.display='block'
+      document.querySelector('.product-page img').src=e
 
   }
   const image="https://www.pakmobizone.pk/wp-content/uploads/2020/09/infinix-Hot-10-Ocean-Wave-5.jpg"
+
+  document.querySelectorAll(".smartphones img").forEach(phone=>{
+        alert("hello")
+      
+  })
 
   return (
     <div className="smartphones-container">
       <h3>Smartphones</h3>
       <div className="smartphones">
-      <div className="smartphone-image">
-       <img src={Iphone} width="" onClick={handleClick} /> 
+      <div >
+       <img src={Iphone} width="" onClick={() =>handleClick(Iphone)} /> 
        <h5>IPhone 12 
        <br/>ksh 17000</h5>
       </div>
       <div>
-        <img src={Huawei} width="200" onClick={handleClick} />
+        <img src={Huawei} width="200" onClick={()=>handleClick(Huawei) } />
         <h5>Huawei P40 Pro
         <br/>ksh 16549</h5>
       </div>
       <div>
-        <img src={Hot10} onClick={handleClick} />
+        <img src={Hot10} onClick={()=>handleClick(Hot10)} />
         <h5>Infinix Hot 10<br/>ksh 8999</h5>
       </div>
       <div>
-         <img src={zero8} onClick={handleClick} />
+         <img src={zero8} onClick={() =>handleClick(zero8)} />
         <h5>Infinix 08 <br/>ksh 9499</h5>
       </div>
       </div>
