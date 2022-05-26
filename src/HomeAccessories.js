fdc39f2878bbdd114f7c87b773c1bfea9a7c35be
kass8mal8 <kassimaly21@gmail.com>
2022-05-26 09:41:14 +0300

    2 files modified

--- src/HomeAccessories.js
+++ src/HomeAccessories.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
 const HomeAccessories=()=>{
-  const image="https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/59/246303/1.jpg?7199"
+  const blacksneaker="https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/59/246303/1.jpg?7199"
+
+  const image="https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/81/568724/1.jpg?6621"
+
   return (
     <div className="homestuff-wrapper">
       <h3>Home accessories and footwear</h3>
       <div className="homestuff">
         <div>
-          <img src={image} width=""/>
+          <img src={blacksneaker} width=""/>
+          <p>Black fashion sneakers <br/>ksh 999</p>
+        </div>
+        <div>
+          <img src={image} />
+          <p>White high profile...</p>
         </div>
       </div>  
     </div>
