4cf3e9013820ebf67077342c476f8371bf2cba58
kass8mal8 <kassimaly21@gmail.com>
2022-05-26 09:12:19 +0300

    1 file modified

--- src/HomeAccessories.js
+++ src/HomeAccessories.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const HomeAccessories=()=>{
+  const image="https://ng.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/59/246303/1.jpg?7199"
   return (
-    <div className="Homestuff">
-      HomeHome accessories 
+    <div className="homestuff-wrapper">
+      <h3>Home accessories and footwear</h3>
+      <div className="homestuff">
+        <div>
+          <img src={image} width=""/>
+        </div>
+      </div>  
     </div>
   )
 }
