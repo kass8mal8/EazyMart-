57b81515def714c4f3308d59b3baa9949b8d42df
kass8mal8 <kassimaly21@gmail.com>
2022-05-27 22:15:08 +0300

    1 file modified

--- src/Smartphones.js
+++ src/Smartphones.js
@@ -5,12 +5,12 @@ import ProductPage from "./ProductPage"
 
 const Smartphones=({count, setCount})=>{
     
-    const Iphone ="https://1.bp.blogspot.com/-XWmUPb6yArc/X92DrUq2-gI/AAAAAAAAI2E/AeODkTL8utkua5_lJn0JdjxmAKc7e-rAwCLcBGAsYHQ/s1788/Iphone%2B12.png"
-    const Huawei="https://androidgreek.com/wp-content/uploads/2021/05/Techno.png"
-    
-    const Hot10="https://www.pakmobizone.pk/wp-content/uploads/2020/03/TECNO-Camon-15-Pro-33.png"
+  const phones=[
+    {src:" https://www.telkomsel.com/sites/default/files/box_media/left/desktop/samsung_S21%2B_device.png",name:"Samsung S21",price:17000,id:1 },  
+    {src:" https://www.pakmobizone.pk/wp-content/uploads/2020/03/TECNO-Camon-15-Pro-33.png",name:"Camon 15 pro",price:12000,id:2 } 
+
+  ]
   
-    const zero8="https://www.telkomsel.com/sites/default/files/box_media/left/desktop/samsung_S21%2B_device.png"
 
   const handleClick=(e)=>{
       document.querySelector('.product-page').classList.toggle('visible')
@@ -33,30 +33,14 @@ const Smartphones=({count, setCount})=>{
       <h3>Smartphones</h3>
       <ProductPage />
       <div className="smartphones">
-      <div >
-       <img src={Iphone} width="" onClick={() =>handleClick(Iphone)} /> 
-       <p>Iphone 12 
-       <br/>ksh 17000</p>
-       <button onClick={handleCount}>add to cart</button>
-      </div>
-      <div>
-        <img src={Huawei} width="200" onClick={()=>handleClick(Huawei) } />
-        <p>Camon 17
-        <br/>ksh 16549</p>
-        <button onClick={handleCount}>add to cart</button>
-      </div>
-      <div>
-        <img src={Hot10} onClick={()=>handleClick(Hot10)} />
-      <p>Camon 15 pro<br/>ksh 8999</p>
-      <button onClick={handleCount}>add to cart</button>
-      </div>
-      <div>
-         <img src={zero8} onClick={() =>handleClick(zero8)} />
-        <p>Samsung S21<br/>ksh 9499</p>
-       <button onClick={handleCount}>add to cart</button>
-      </div>
-      </div>
-
+        {phones.map(phone=>(
+           <div>
+             <img src={phone.src} />
+             <p>{phone.name} <br/>ksh {phone.price} </p>
+             <button onClick={() =>handleClick(phone.src)}>add to cart</button>
+           </div>
+        ))}
+        </div>
         <div className="product-page">
           <FontAwesomeIcon icon={faClose} onClick={handleClose} id="product-close"></FontAwesomeIcon>
           <img src={image} />
