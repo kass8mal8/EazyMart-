755aa3ab85ec880d1bb07d4b72bf1f4b7f872b3b
kass8mal8 <kassimaly21@gmail.com>
2022-05-27 16:07:44 +0300

    2 files modified

--- src/About.js
+++ src/About.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import emailjs from '@emailjs/browser';
 
-const About=()=>{
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
-    <div></div>
-  )
-}
-export default About
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
