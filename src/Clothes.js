d7cb40ae62e1ffc29e34a49f1e6912a921cb2942
kass8mal8 <kassimaly21@gmail.com>
2022-05-28 14:45:42 +0300

    1 file modified

--- src/Clothes.js
+++ src/Clothes.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Clothes=()=>{
   const images=[
-    {src:"https://pngimg.com/uploads/jacket/jacket_PNG8057.png",name:"jacket"},
-    {src:"https://pngimg.com/uploads/jacket/jacket_PNG8057.png", name:"dress"}
+    {src:"https://pngimg.com/uploads/jacket/jacket_PNG8057.png",name:"Jacket",price:499, id:1 },
+    {src:"https://www.pngpix.com/wp-content/uploads/2016/10/PNGPIX-COM-Suit-PNG-Transparent-Image-250x300.png", name:"Black suite",price:1599, id:2}, 
+    {src:"https://cdn.shopify.com/s/files/1/1144/7088/files/RUSEEN_Reflective_Apparel-Reflective_Shirt-Unisex-Polyester-Birdseye_Knit-Orange-Long_Sleeve-Reflective_Running_Gear-Reflective_Clothing-Blank-Front.png?1469418611344383116", name:"Long sleeve shirt",price:250, id:3}, 
+    {src:"https://pluspng.com/img-png/clothes-png-hd-dress-shirt-png-hd-png-image-480.png", name:"Shirt",price:99, id:4} 
+
+
   ]
   return(
     <div>
@@ -13,7 +17,8 @@ const Clothes=()=>{
       {images.map(image=>(
          <div>
          <img src={image.src} />
-         <p>{image.name}</p>
+         <p>{image.name}<br/>ksh {image.price}</p>
+         <button>add to cart</button>
         </div>
       )) }
     </div>
