8d310e52991ea7c5adcaffc5e698dc392eee4556
kass8mal8 <kassimaly21@gmail.com>
2022-05-28 20:36:51 +0300

    2 files modified

--- src/Smartphones.js
+++ src/Smartphones.js
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
-import { faClose, faHeart } from '@fortawesome/free-solid-svg-icons' 
+import { faClose } from '@fortawesome/free-solid-svg-icons' 
+import { faHeart } from '@fortawesome/free-regular-svg-icons' 
+
 import ProductPage from "./ProductPage"
 
 const Smartphones=({count, setCount})=>{
