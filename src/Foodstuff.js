e0bad9ab5b31503fa8eb40662f3113b3d8c5e5b2
kass8mal8 <kassimaly21@gmail.com>
2022-05-30 08:23:08 +0300

    1 file modified

--- src/Foodstuff.js
+++ src/Foodstuff.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Foodstuff =()=>{
 const foods=[
-  {src:"https://capwell.co.ke/wp-content/uploads/2020/03/pearl-pishori.png", name:"Pearl-pishori"}
+  {src:"https://capwell.co.ke/wp-content/uploads/2020/03/pearl-pishori.png", name:"Pearl-pishori", price:250, id:1},
+  {src:"https://unga-group.com/wp-content/uploads/2019/06/Jogoo-1-300x232.png", name:"Jogoo Unga", price:150, id:2},
+  {src:"https://mybigorder.com/public/uploads/products/thumbnail/bUF9ShhoBlBqUTGOtipyj8TWFMydTxVQUggMzsXx.png", name:"Taifa ngano", price:200, id:3},
+  {src:"https://giantmillers.co.ke/wp-content/uploads/2020/03/canna_maize_meal.png", name:"Canna maize", price:250, id:3},
+
 ]
   return(
     <div className ="container">
@@ -12,7 +16,7 @@ const foods=[
       <div>
 
             <img src={food.src} alt="" />
-            <p>{food.name}<br/>sh {food.price} </p>
+            <p>{food.name}<br/>ksh {food.price} </p>
         </div>
       ))}
       </div>
