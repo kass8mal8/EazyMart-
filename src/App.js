969ccc3b631f7e22ba088ef28fec7366375877f7
kass8mal8 <kassimaly21@gmail.com>
2022-05-30 21:27:58 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState } from "react";
 import "./style.css";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './Header'
--- src/Smartphones.js
+++ src/Smartphones.js
@@ -1,7 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+import React,{useSt} from 'react'
 import { faClose } from '@fortawesome/free-solid-svg-icons' 
-import { faHeart } from '@fortawesome/free-regular-svg-icons' 
 
 import ProductPage from "./ProductPage"
 
