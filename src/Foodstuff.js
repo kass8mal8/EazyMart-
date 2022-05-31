797ffb5eee88393b3e89b16d4462fe9df47cbd37
kass8mal8 <kassimaly21@gmail.com>
2022-05-31 11:45:10 +0300

    1 file modified

--- src/Foodstuff.js
+++ src/Foodstuff.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Foodstuff =({count, set})=>{
+const Foodstuff =({count, setount})=>{
 const foods=[
   {src:"https://megawholesalers.co.ke/images/stories/virtuemart/product/PACK-EXE-All-Purpose-e1429515425104.png", name:"Exe Flour", price:140, id:1},
   {src:"https://unga-group.com/wp-content/uploads/2019/06/Jogoo-1-300x232.png", name:"Jogoo Unga", price:150, id:2},
   {src:"https://mybigorder.com/public/uploads/products/thumbnail/bUF9ShhoBlBqUTGOtipyj8TWFMydTxVQUggMzsXx.png", name:"Taifa ngano", price:200, id:3},
-  {src:"https://giantmillers.co.ke/wp-content/uploads/2020/03/canna_maize_meal.png", name:"Canna maize", price:250, id:},
+  {src:"https://giantmillers.co.ke/wp-content/uploads/2020/03/canna_maize_meal.png", name:"Canna maize", price:250, id:4},
 
 ]
 const handleCount =()=>{
