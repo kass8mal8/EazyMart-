75f9c019b12f64aa4574ddd55644aec0fb80430c
kass8mal8 <kassimaly21@gmail.com>
2022-05-31 09:19:08 +0300

    1 file modified

--- src/About.js
+++ src/About.js
@@ -1,5 +1,5 @@
 import emailjs from '@emailjs/browser';
-import React, {useRef} from 'react'
+import React, {useRef,useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
   import { faTwitter, faFacebookSquare,  faInstagram } from '@fortawesome/free-brands-svg-icons' 
   import { faClose } from '@fortawesome/free-solid-svg-icons' 
@@ -8,9 +8,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
  const About = () => {
   const form = useRef();
 
+  const [isHide,setIsHide ]=useState(false)
   const sendEmail = (e) => {
     e.preventDefault();
-
+   setIsHide(true)
     emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
       .then((result) => {
           console.log(result.text);
@@ -21,10 +22,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
   return (
     <div className="container">
-      <div className ="success-msg">
+      {isHide && <div className ="success-msg">
         <p>message sent successfuly</p>
        <FontAwesomeIcon icon={faClose}></FontAwesomeIcon>
-      </div>
+      </div>} 
       <p className ="about-paragraph">We are EazyMart your convenient shopping partner of all time. We have great offers just for you can't wait to have you shop with us. </p>
       <p className ="about-paragraph">Don't hesitate to reach out to us for any queries and complaints, drop your message below. </p>
     <form ref={form} onSubmit={sendEmail}>
