88b716b9bb581d03ad1a560777c098e4dfc2462b
kass8mal8 <kassimaly21@gmail.com>
2022-05-31 11:42:00 +0300

    1 file modified

--- src/Foodstuff.js
+++ src/Foodstuff.js
@@ -5,7 +5,7 @@ const foods=[
   {src:"https://megawholesalers.co.ke/images/stories/virtuemart/product/PACK-EXE-All-Purpose-e1429515425104.png", name:"Exe Flour", price:140, id:1},
   {src:"https://unga-group.com/wp-content/uploads/2019/06/Jogoo-1-300x232.png", name:"Jogoo Unga", price:150, id:2},
   {src:"https://mybigorder.com/public/uploads/products/thumbnail/bUF9ShhoBlBqUTGOtipyj8TWFMydTxVQUggMzsXx.png", name:"Taifa ngano", price:200, id:3},
-  {src:"https://giantmillers.co.ke/wp-content/uploads/2020/03/canna_maize_meal.png", name:"Canna maize", price:250, id:3},
+  {src:"https://giantmillers.co.ke/wp-content/uploads/2020/03/canna_maize_meal.png", name:"Canna maize", price:250, id:},
 
 ]
 const handleCount =()=>{
