ec5792a3ae883e289f1cdeb8f82ecd2a26279f7a
kass8mal8 <kassimaly21@gmail.com>
2022-06-01 22:15:39 +0300

    1 file modified

--- src/Mainproducts.js
+++ src/Mainproducts.js
@@ -6,6 +6,7 @@ import Foodstuff from './Foodstuff  '
 
 const Mainproducts=()=>{
   const [count, setCount]=useState(0)
+  const [isShown,setIsShown]=useState(false)
   return(
     <div className="container">
       <small>{count}</small>
@@ -14,10 +15,9 @@ const Mainproducts=()=>{
          <p>Shopping experience like no other and with bigger offers than never.Enjoy our products at discounted prices.</p>
        </section>
        <Foodstuff count={count}  setCount ={setCount} />
-       <Smartphones count={count} setCount={setCount} />
+       <Smartphones count={count} setCount={setCount} isShown={isShown} setIsShown={setIsShown}/>
        <Clothes count={count} setCount={setCount} />
        <HomeAccessories count={count} setCount={setCount} />
-
     </div>   
   )
 }
