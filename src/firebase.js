4c45d340464af8d800d5a11ea93111a662f007c4
kass8mal8 <kassimaly21@gmail.com>
2022-06-05 16:25:55 +0300

    1 file modified

--- src/firebase.js
+++ src/firebase.js
@@ -17,3 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth=getAuth()
+
+export function signup(email, password){
+   createUserWithEmailAndPassword(auth, email, password)
+}
