61447a8375aa0815b45c1c6a05e173d0c8289b28
kass8mal8 <kassimaly21@gmail.com>
2022-06-06 10:06:07 +0300

    1 file modified

--- src/Signup.js
+++ src/Signup.js
@@ -8,10 +8,10 @@ const Signup=({setCreated})=>{
   const passwordRef=useRef()
 
   async function handleSignup(){
-    setIsPending(true)
      try{
        await signup(emailRef.current.value, passwordRef.current.value)
        setCreated(true)
+    setIsPending(true)
      }
      catch{
        alert('error')
