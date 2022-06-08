36dc246474b7c7eb35c8fad5dc22c94e9175a7ae
kass8mal8 <kassimaly21@gmail.com>
2022-06-08 18:04:10 +0300

    1 file modified

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,25 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+ import { initializeApp } from "firebase/app";
+ import {getAuth, createUserWithEmailAndPassword} from "firebase/auth"
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCIMC-BGvBeO4VUg7DbPJq-F0Z_z87LyAM",
-  authDomain: "eazy-mart-auth.firebaseapp.com",
-  projectId: "eazy-mart-auth",
-  storageBucket: "eazy-mart-auth.appspot.com",
-  messagingSenderId: "749499150162",
-  appId: "1:749499150162:web:f49130694ac44b3c40948b"
-};
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+     apiKey: "AIzaSyCIMC-BGvBeO4VUg7DbPJq-F0Z_z87LyAM",
+       authDomain: "eazy-mart-auth.firebaseapp.com",
+         projectId: "eazy-mart-auth",
+           storageBucket: "eazy-mart-auth.appspot.com",
+             messagingSenderId: "749499150162",
+               appId: "1:749499150162:web:f49130694ac44b3c40948b"
+ };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const auth=getAuth()
-export const provider=new GoogleAuthProvider()
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ const auth=getAuth()
 
+ export function signup(email, password) {
+        return createUserWithEmailAndPassword(auth, email, password) 
+ } 
+
+  
+  
+ }
+ }
