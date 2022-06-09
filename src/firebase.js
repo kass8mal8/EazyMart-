f259cdd7ac405b474d7a1b8002b440bd3a412f30
Kassim Ali <105120332+kass8mal8@users.noreply.github.com>
2022-06-09 08:24:25 +0300

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,11 +4,11 @@
  // Your web app's Firebase configuration
  const firebaseConfig = {
      apiKey: "AIzaSyCIMC-BGvBeO4VUg7DbPJq-F0Z_z87LyAM",
-       authDomain: "eazy-mart-auth.firebaseapp.com",
-         projectId: "eazy-mart-auth",
-           storageBucket: "eazy-mart-auth.appspot.com",
-             messagingSenderId: "749499150162",
-               appId: "1:749499150162:web:f49130694ac44b3c40948b"
+     authDomain: "eazy-mart-auth.firebaseapp.com",
+     projectId: "eazy-mart-auth",
+     storageBucket: "eazy-mart-auth.appspot.com",
+     messagingSenderId: "749499150162",
+     appId: "1:749499150162:web:f49130694ac44b3c40948b"
  };
 
  // Initialize Firebase
@@ -20,6 +20,3 @@
  } 
 
   
-  
- }
- }
