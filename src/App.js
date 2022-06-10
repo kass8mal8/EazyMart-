f090d9878cc430a2320f13dc43c039033812c9ea
kassimaly21@gmail.com <kassimaly21@gmail.com>
2022-06-10 23:09:34 +0300

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState } from "react";
 import "./style.css";
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Header from './Header'
 import Mainproducts from "./Mainproducts"
 import About from './About'
@@ -35,34 +35,8 @@ export default function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header handleCart={handleCart} isUserCreated={isUserCreated}  setIsUserCreated={setIsUserCreated} user={user} />
-         <div className="cart-content">
-          <div style={{borderBottom:'2px solid #ccc',fontWeight:'bold'}}>
-            <p>Cart</p>
-          </div>
+        <Header handleCart={handleCart}  isUserCreated={isUserCreated}  setIsUserCreated={setIsUserCreated} user={user} count={count} />
           
-         {count < 1 ?
-         <p style={{color:'gray',
-           textAlign:'center',
-           marginTop:'22%'}}>
-           You have no items in your cart
-         </p> :
-         <div>
-            <p style={{marginTop:'5%', textAlign:'center'}} >You have {count} items in your cart</p>
-            <Link to="/cart" >
-            <button style={{background:'hsl(25,100%,60%)',
-              border:'1px solid hsl(25,100%,60%)',
-              width:'100%',
-              marginTop:'15%',
-              padding:'10px',
-              fontFamily:'kumbh sans',
-              borderRadius:'2px'}}>proceed to checkout
-             </button>
-             </Link>
-         </div>}
-           
-        </div>
-        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars,faCartShopping, faClose, } from '@fortawesome/free-solid-svg-icons' 
 import "./style.css"
@@ -6,7 +7,7 @@ import {Link}  from 'react-router-dom'
 import {auth} from './firebase'
 import {signOut} from 'firebase/auth'
 
-const Header =({handleCart,isUserCreated,setIsUserCreated, user})=>{
+const Header =({handleCart,isUserCreated,setIsUserCreated, user, count})=>{
 
   const handleToggle=()=>{
     document.getElementById("toggle").classList.toggle("active")
@@ -25,10 +26,38 @@ const Header =({handleCart,isUserCreated,setIsUserCreated, user})=>{
   }
   
   
-  
   return(
     <div>
         <div className="h-div">
+        
+         <div className="cart-content">
+            <div style={{borderBottom:'2px solid #ccc',fontWeight:'bold'}}>
+                  <p>Cart</p>
+            </div>
+         {count < 1 ?
+         <p style={{color:'gray',
+           textAlign:'center',
+           marginTop:'22%'}}>
+           You have no items in your cart
+         </p> :
+         <div>
+            <p style={
+            {marginTop:'5%',
+            textAlign:'center'}} >You have {count} items in your cart
+            </p>
+            <Link to="/cart" >
+            <button style={{background:'hsl(25,100%,60%)',
+              border:'1px solid hsl(25,100%,60%)',
+              width:'100%',
+              marginTop:'15%',
+              padding:'10px',
+              fontFamily:'kumbh sans',
+              borderRadius:'2px'}}>proceed to checkout
+             </button>
+             </Link>
+         </div>}
+         </div>
+        
         
         <FontAwesomeIcon icon={faBars} className="h-div--bars" onClick={handleToggle}></FontAwesomeIcon>
         <h3>EazyMart</h3>
