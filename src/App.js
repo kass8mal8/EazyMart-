1928a6fe0c61fa920ce07d4a7f04ac7047767101
kassimaly21@gmail.com <kassimaly21@gmail.com>
2022-06-11 08:02:52 +0300

    app.js, and header.js modified, cart.js removed

--- src/App.js
+++ src/App.js
@@ -36,6 +36,35 @@ export default function App() {
     <div>
       <BrowserRouter>
         <Header handleCart={handleCart}  isUserCreated={isUserCreated}  setIsUserCreated={setIsUserCreated} user={user} count={count} />
+                  
+        <div className="cart-content">
+            <div style={{borderBottom:'2px solid #ccc',fontWeight:'bold'}}>
+                  <p>Cart</p>
+            </div>
+         {count < 1 ?
+         <p style={{color:'gray',
+           textAlign:'center',
+           marginTop:'22%'}}>
+           You have no items in your cart
+         </p> :
+         <div>
+            <p style={
+            {marginTop:'5%',
+            textAlign:'center'}} >You have {count} items in your cart
+            </p>
+            <Link to="/cart" >
+            <button style={{background:'hsl(25,100%,60%)',
+              border:'1px solid hsl(25,100%,60%)',
+              width:'100%',
+              marginTop:'15%',
+              padding:'10px',
+              fontFamily:'kumbh sans',
+              borderRadius:'2px'}}>proceed to checkout
+             </button>
+             </Link>
+         </div>}
+         </div>
+ 
           
         <Routes>
           <Route path="/" element={<Home />} />
